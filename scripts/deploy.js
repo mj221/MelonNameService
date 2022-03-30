9719efbfcb22abd959432fd1fc2d2ab66c5c7608
MJ LEE <mj1201276@yahoo.com.au>
2022-03-30 16:45:33 +1000

    Added a contract address exporter for convenience

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,46 @@
+
+const fs = require('fs');
+
 const main = async () =>{
 	const domainContractFactory = await hre.ethers.getContractFactory('Domains')
 	const domainContract = await domainContractFactory.deploy("melon")
 	await domainContract.deployed()
 
 	console.log("Contract deployed to: ", domainContract.address)
+	saveFrontendFiles(domainContract.address)
+	// let path = "QmWcXxJXsdD94msN5L6iu8YeEzhcKvDJap82tmRrppaw7Z"
+	// let ipfspath = `ipfs://${path}`
 
-	let txn = await domainContract.register("diamond", {value: hre.ethers.utils.parseEther('0.1'), gasLimit: '10429720'})
-	await txn.wait()
+	// let txn = await domainContract.register("github", ipfspath, {value: hre.ethers.utils.parseEther('0.1'), gasLimit: '10429720'})
+	// await txn.wait()
 
-	console.log("Minted domain diamond.melon")
+	// console.log("Minted domain github.melon")
 
-	txn = await domainContract.setRecord("diamond", "A diamond handed melon")
-	await txn.wait()
-	const address = await domainContract.getAddress("diamond")
-	console.log("Owner of domain diamond: ", address)
+	// txn = await domainContract.setRecord("github", "A github melon")
+	// await txn.wait()
+	// const address = await domainContract.getAddress("github")
+	// console.log("Owner of domain github: ", address)
 
-	const balance = await hre.ethers.provider.getBalance(domainContract.address)
-	console.log("Contract balance:", hre.ethers.utils.formatEther(balance))
+	// const balance = await hre.ethers.provider.getBalance(domainContract.address)
+	// console.log("Contract balance:", hre.ethers.utils.formatEther(balance))
+}
+
+async function saveFrontendFiles(tokenAddr) {
+  const contractsDir = __dirname + "/../domain-melon/src/build/contracts";
+
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
+
+  fs.writeFileSync(
+    contractsDir + "/contract-address.json",
+    JSON.stringify(
+      {
+        MNSContractAddress: tokenAddr
+      }
+    )
+  );
+
 }
 
 const runMain = async () =>{
